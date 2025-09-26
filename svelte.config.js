ffd44e9302badf9ab40e3b65c44fe7959f1fa569
Gabriel Martín Moran <gabriel.moran@fluxit.com.ar>
2025-09-26 19:03:28 -0300

    Add custom 404 page and set base path for GitHub Pages deployment

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,18 @@
 import adapter from '@sveltejs/adapter-static';
 
+// El nombre de tu repositorio de GitHub
+const dev = process.argv.includes('dev');
+const base = dev ? '' : '/proyecto-arcana';
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		adapter: adapter({
-			fallback: 'index.html', // may differ from host to host
+			fallback: '404.html',
 		}),
+		paths: {
+			base: base,
+		},
 	},
 };
 
