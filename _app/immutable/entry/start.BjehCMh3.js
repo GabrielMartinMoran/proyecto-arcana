e32e85494c3cb9ef4109d2814ace4e7e621eace1
GabrielMartinMoran <GabrielMartinMoran@users.noreply.github.com>
2025-09-26 20:21:08 +0000

    deploy: 84a43e452464ddda02459be931234c78ee04f9ef

---
+++ _app/immutable/entry/start.BjehCMh3.js
@@ -0,0 +1,1 @@
+import{l as o,a as r}from"../chunks/_f5P3yG9.js";export{o as load_css,r as start};
---
+++ _app/immutable/nodes/7.Hn-lw0FS.js
@@ -0,0 +1,1 @@
+import"../chunks/DsnmJJEf.js";import"../chunks/GLbBk91X.js";import{f as a,b as e,s as t,d as i,r as n}from"../chunks/Dq1J35Nh.js";import{M as c}from"../chunks/B1Nfq8cK.js";var m=a('<section class="svelte-1gsaei5"><h1>Manual del Director de Juego</h1> <!></section>');function g(s){var o=m(),r=t(i(o),2);c(r,{src:"/docs/gm.md"}),n(o),e(s,o)}export{g as component};
