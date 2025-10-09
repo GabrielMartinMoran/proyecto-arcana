cdb01253b7c95283860b49e9d80976f90ec05cd8
GabrielMartinMoran <GabrielMartinMoran@users.noreply.github.com>
2025-10-09 16:29:37 +0000

    deploy: a55d0becc5249ed77f2e6ea8122e7547dd7321bd

---
+++ _app/immutable/nodes/2.CYUmPg8D.js
@@ -0,0 +1,1 @@
+import{a as e,_ as n}from"../chunks/CW6do3lq.js";export{e as component,n as universal};
--- _app/immutable/nodes/2.DWVmmJZh.js
+++
@@ -1,1 +0,0 @@
-import{a as e,_ as n}from"../chunks/D_Zbr8QM.js";export{e as component,n as universal};
