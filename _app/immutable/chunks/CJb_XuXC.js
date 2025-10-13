59e9351df14400119062c6e45f025faac4bfe358
GabrielMartinMoran <GabrielMartinMoran@users.noreply.github.com>
2025-10-13 13:48:15 +0000

    deploy: cfdf919cda0358aa31f4e3e7dc692f2a04231410

---
+++ _app/immutable/chunks/CJb_XuXC.js
@@ -0,0 +1,1 @@
+import{b as e,r as o}from"./Bur0yID0.js";function t(...r){return e+o(r[0],r[1])}export{t as r};
--- _app/immutable/nodes/2.Dqjk_FCF.js
+++
@@ -1,1 +0,0 @@
-import{a as e,_ as n}from"../chunks/Bgfmf6Yg.js";export{e as component,n as universal};
