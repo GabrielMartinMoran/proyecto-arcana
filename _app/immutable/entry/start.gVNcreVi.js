1dd9c9a3a7debfb6210b3821e00df1e98fe59879
GabrielMartinMoran <GabrielMartinMoran@users.noreply.github.com>
2026-05-04 17:49:11 +0000

    deploy: 19bbfa3a724fe0488126eba217c81ccc0e718d9f

---
+++ _app/immutable/entry/start.gVNcreVi.js
@@ -0,0 +1,1 @@
+import{l as o,a as r}from"../chunks/BRIV_qpk.js";export{o as load_css,r as start};
---
+++ _app/immutable/nodes/2.CxwfZ4ha.js
@@ -0,0 +1,1 @@
+import{a as e,_ as n}from"../chunks/DqB8uksK.js";export{e as component,n as universal};
