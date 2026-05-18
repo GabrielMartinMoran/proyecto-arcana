203981df19f1631b330268f058422ba6f002ad40
GabrielMartinMoran <GabrielMartinMoran@users.noreply.github.com>
2026-05-18 17:29:33 +0000

    deploy: c86a43c3c603b7792662cdf0976a57547badb8af

--- _app/immutable/entry/start.BtlTGWZ4.js
+++
@@ -1,1 +0,0 @@
-import{l as o,a as r}from"../chunks/Cr07ExW9.js";export{o as load_css,r as start};
---
+++ _app/immutable/nodes/2.C2K4XIZr.js
@@ -0,0 +1,1 @@
+import{a as e,_ as n}from"../chunks/B5X-41Rz.js";export{e as component,n as universal};
