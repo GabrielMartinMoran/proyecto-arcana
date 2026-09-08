fc46a2c6b2414a427ecda705afaa0e0047cfba6f
GabrielMartinMoran <GabrielMartinMoran@users.noreply.github.com>
2026-09-08 20:48:17 +0000

    deploy: b5a85a78acf841d89a5e41d9338fe9ed40dc235b

---
+++ _app/immutable/nodes/2.BkMKXAm4.js
@@ -0,0 +1,1 @@
+import{a as e,_ as n}from"../chunks/CD5WuDyB.js";export{e as component,n as universal};
--- _app/immutable/nodes/2.swC7UM3J.js
+++
@@ -1,1 +0,0 @@
-import{a as e,_ as n}from"../chunks/CIqKuM5X.js";export{e as component,n as universal};
