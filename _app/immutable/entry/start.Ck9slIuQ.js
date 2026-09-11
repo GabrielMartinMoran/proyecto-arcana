50217b286956fc4cfb21329c934f446177ed7fef
GabrielMartinMoran <GabrielMartinMoran@users.noreply.github.com>
2026-09-11 19:25:11 +0000

    deploy: 657fc20b2a71c36921690dc1cd4c174e7225deff

--- _app/immutable/entry/start.Ck9slIuQ.js
+++
@@ -1,1 +0,0 @@
-import{l as o,a as r}from"../chunks/CeEI4OuI.js";export{o as load_css,r as start};
---
+++ _app/immutable/nodes/2.C3Rf8Ge-.js
@@ -0,0 +1,1 @@
+import{a as e,_ as n}from"../chunks/DZ2OweKr.js";export{e as component,n as universal};
